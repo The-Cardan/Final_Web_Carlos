51db2e3ccbbce0aae36909d327420099d4734f6e
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 01:43:27 +0200

    Create PacienteUpdateDto.cs

---
+++ DTOs/PacienteUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class PacienteUpdateDto
+    {
+        [Required]
+        [StringLength(120)]
+        public string NombreCompleto { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(15)]
+        public string Cedula { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        public string Telefono { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string? Correo { get; set; }
+
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
