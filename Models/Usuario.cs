22fa97bbef91279b71b4943cce5d60f6b2c26a76
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 02:43:08 +0200

    Usuario.cs
    Usada para la clase que hará posible el inicio de sesión

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,21 @@
-﻿namespace Final_Web_Carlos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.Models
 {
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string Correo { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
