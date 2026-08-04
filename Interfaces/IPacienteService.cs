0fc8d761c59f871f703e7f4bd5b9dcf51b4e5afe
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 01:53:33 +0200

    Create IPacienteService.cs

---
+++ Interfaces/IPacienteService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IPacienteService
+    {
+        Task<List<PacienteResponseDto>> ObtenerTodosAsync();
+
+        Task<PacienteResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<PacienteResponseDto> CrearAsync(PacienteCreateDto dto);
+
+        Task<PacienteResponseDto?> ActualizarAsync(int id, PacienteUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
