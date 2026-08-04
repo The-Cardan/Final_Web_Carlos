b4f7449197e489353db56f76cf6434d8f0718ab0
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 04:21:49 +0200

    Update UsuarioRegisterDto.cs
    Registro de usuario

--- DTOs/UsuarioRegisterDto.cs
+++ DTOs/UsuarioRegisterDto.cs
@@ -1,6 +1,19 @@
-﻿namespace Final_Web_Carlos.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
 {
     public class UsuarioRegisterDto
     {
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
     }
 }
