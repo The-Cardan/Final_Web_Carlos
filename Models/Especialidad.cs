ff04543df62f14710b974e6e5448f07a3fa276d8
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 02:47:16 +0200

    Update Especialidad.cs
    Relación, una especialidad puede tener muchos dentistas

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -1,6 +1,17 @@
-﻿namespace Final_Web_Carlos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.Models
 {
     public class Especialidad
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        // Relación: Una especialidad puede tener muchos dentistas
+        public ICollection<Dentista>? Dentistas { get; set; }
     }
 }
