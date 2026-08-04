686563cded1f761a64f92eb2c3957b2e6d75f744
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 04:23:15 +0200

    Update UsuarioResponseDto.cs

--- DTOs/UsuarioResponseDto.cs
+++ DTOs/UsuarioResponseDto.cs
@@ -2,5 +2,10 @@
 {
     public class UsuarioResponseDto
     {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
     }
 }
