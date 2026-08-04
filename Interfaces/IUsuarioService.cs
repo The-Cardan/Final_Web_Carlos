421a91addca5727b2d30451848e255271dc0bc1a
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 05:35:41 +0200

    Update IUsuarioService.cs

--- Interfaces/IUsuarioService.cs
+++ Interfaces/IUsuarioService.cs
@@ -1,6 +1,17 @@
-﻿namespace Final_Web_Carlos.Interfaces
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
 {
-    public class IUsuarioService
+    public interface IUsuarioService
     {
+        Task<UsuarioResponseDto> RegistrarAsync(UsuarioRegisterDto dto);
+
+        Task<UsuarioResponseDto?> LoginAsync(UsuarioLoginDto dto);
+
+        Task<List<UsuarioResponseDto>> ObtenerUsuariosAsync();
+
+        Task<UsuarioResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<bool> EliminarAsync(int id);
     }
 }
