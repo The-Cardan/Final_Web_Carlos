90d7e9860530c251529d7994f9563a95192917da
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 05:25:33 +0200

    Update IUsuarioRepository.cs
    Porque todas las operaciones de base de datos deben ser asíncronas.

--- Interfaces/IUsuarioRepository.cs
+++ Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace Final_Web_Carlos.Interfaces
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
 {
-    public class IUsuarioRepository
+    public interface IUsuarioRepository
     {
+        Task<Usuario?> ObtenerPorCorreoAsync(string correo);
+
+        Task<Usuario?> ObtenerPorIdAsync(int id);
+
+        Task<List<Usuario>> ObtenerTodosAsync();
+
+        Task<Usuario> CrearAsync(Usuario usuario);
+
+        Task<Usuario> ActualizarAsync(Usuario usuario);
+
+        Task<bool> EliminarAsync(int id);
+
+        Task<bool> ExisteCorreoAsync(string correo);
     }
 }
