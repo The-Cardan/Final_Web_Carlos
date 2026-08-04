46d535239693c208fee47339f79342e7bf92ada0
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-04 02:44:35 +0200

    Paciente.cs
    Tabla para información del paciente

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -1,6 +1,27 @@
-﻿namespace Final_Web_Carlos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.Models
 {
     public class Paciente
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(120)]
+        public string NombreCompleto { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(15)]
+        public string Cedula { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        public string Telefono { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string? Correo { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
     }
 }
