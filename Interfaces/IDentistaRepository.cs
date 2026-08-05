52fa8440487c0570db22631ece63c82210c4f3b1
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:47:07 +0200

    Create IDentistaRepository.cs

---
+++ Interfaces/IDentistaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IDentistaRepository
+    {
+        Task<List<Dentista>> ObtenerTodosAsync();
+
+        Task<Dentista?> ObtenerPorIdAsync(int id);
+
+        Task<Dentista> CrearAsync(Dentista dentista);
+
+        Task<Dentista> ActualizarAsync(Dentista dentista);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
