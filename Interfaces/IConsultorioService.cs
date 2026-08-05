2ea93e4d208749e15039de04938d410c51c6b782
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:20:33 +0200

    Create IConsultorioService.cs

---
+++ Interfaces/IConsultorioService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IConsultorioService
+    {
+        Task<List<ConsultorioResponseDto>> ObtenerTodosAsync();
+
+        Task<ConsultorioResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<ConsultorioResponseDto> CrearAsync(ConsultorioCreateDto dto);
+
+        Task<ConsultorioResponseDto?> ActualizarAsync(int id, ConsultorioUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
