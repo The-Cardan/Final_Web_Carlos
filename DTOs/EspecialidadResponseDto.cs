6b3f28f195d2f72c72d42012a07b24d97406a707
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:26:47 +0200

    Create EspecialidadResponseDto.cs

---
+++ DTOs/EspecialidadResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class EspecialidadResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
