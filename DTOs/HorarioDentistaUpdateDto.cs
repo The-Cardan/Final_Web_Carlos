79bf51000693495a556bad6fdab861e76a9d8083
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:28:40 +0200

    Update HorarioDentistaUpdateDto.cs

--- DTOs/HorarioDentistaUpdateDto.cs
+++ DTOs/HorarioDentistaUpdateDto.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Final_Web_Carlos.Models.Enums;
 
 namespace Final_Web_Carlos.DTOs
 {
     public class HorarioDentistaUpdateDto
     {
         [Required]
-        public DayOfWeek Dia { get; set; }
+        public DiaSemana Dia { get; set; }
 
         [Required]
         public TimeSpan HoraInicio { get; set; }
