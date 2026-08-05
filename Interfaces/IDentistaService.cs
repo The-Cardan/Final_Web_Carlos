0d73144c5a1dcaf606e0b203505d68eb777699aa
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:47:48 +0200

    Create IDentistaService.cs

---
+++ Interfaces/IDentistaService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IDentistaService
+    {
+        Task<List<DentistaResponseDto>> ObtenerTodosAsync();
+
+        Task<DentistaResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<DentistaResponseDto> CrearAsync(DentistaCreateDto dto);
+
+        Task<DentistaResponseDto?> ActualizarAsync(int id, DentistaUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
