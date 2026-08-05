c0337a395bbaf1f33c254f3fd97c77fb03565360
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:09:17 +0200

    Create IServicioService.cs

---
+++ Interfaces/IServicioService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IServicioService
+    {
+        Task<List<ServicioResponseDto>> ObtenerTodosAsync();
+
+        Task<ServicioResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<ServicioResponseDto> CrearAsync(ServicioCreateDto dto);
+
+        Task<ServicioResponseDto?> ActualizarAsync(int id, ServicioUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
