79c43d7d4c6d98884e350d61397ac2806db02cbe
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:47:23 +0200

    Create CitaHelper.cs

---
+++ Helpers/CitaHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace Final_Web_Carlos.Helpers
+{
+    public static class CitaHelper
+    {
+        public static string CalcularEstado(DateTime fechaHoraInicio, DateTime fechaHoraFin)
+        {
+            var ahora = DateTime.Now;
+
+            if (ahora < fechaHoraInicio)
+                return "Vigente";
+
+            if (ahora >= fechaHoraInicio && ahora <= fechaHoraFin)
+                return "En proceso";
+
+            return "Finalizado";
+        }
+
+        public static string CalcularTiempoRestante(DateTime fechaHoraInicio)
+        {
+            var diferencia = fechaHoraInicio - DateTime.Now;
+
+            if (diferencia.TotalSeconds <= 0)
+                return "Finalizada";
+
+            int dias = diferencia.Days;
+            int horas = diferencia.Hours;
+            int minutos = diferencia.Minutes;
+
+            if (dias > 0)
+                return $"{dias} días {horas} horas";
+
+            if (horas > 0)
+                return $"{horas} horas {minutos} minutos";
+
+            return $"{minutos} minutos";
+        }
+    }
+}
