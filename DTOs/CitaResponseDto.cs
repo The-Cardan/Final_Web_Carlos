a3f150c56318b9d938c3cde0572f60ef3c963d20
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:42:34 +0200

    Create CitaResponseDto.cs

---
+++ DTOs/CitaResponseDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class CitaResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Paciente { get; set; } = string.Empty;
+
+        public string Dentista { get; set; } = string.Empty;
+
+        public string Especialidad { get; set; } = string.Empty;
+
+        public string Motivo { get; set; } = string.Empty;
+
+        public string Servicio { get; set; } = string.Empty;
+
+        public string Consultorio { get; set; } = string.Empty;
+
+        public DateTime Fecha { get; set; }
+
+        public TimeSpan Hora { get; set; }
+
+        public int Duracion { get; set; }
+
+        public string Estado { get; set; } = string.Empty;
+
+        public string DiasHorasRestantes { get; set; } = string.Empty;
+    }
+}
