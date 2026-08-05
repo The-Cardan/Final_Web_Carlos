078bd5d19636590c7004e7785ae725b8a48b4d39
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:43:17 +0200

    Create ICitaRepository.cs

---
+++ Interfaces/ICitaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface ICitaRepository
+    {
+        Task<List<Cita>> ObtenerTodasAsync();
+
+        Task<Cita?> ObtenerPorIdAsync(int id);
+
+        Task<Cita> CrearAsync(Cita cita);
+
+        Task<Cita> ActualizarAsync(Cita cita);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
