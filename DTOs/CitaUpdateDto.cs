bcf0647f182d070f73b1325a168d5a202d97770b
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:41:51 +0200

    Create CitaUpdateDto.cs

---
+++ DTOs/CitaUpdateDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class CitaUpdateDto
+    {
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        public TimeSpan Hora { get; set; }
+
+        [Required]
+        [Range(1, 480)]
+        public int Duracion { get; set; }
+
+        [Required]
+        public int DentistaId { get; set; }
+
+        [Required]
+        public int MotivoId { get; set; }
+
+        [Required]
+        public int ServicioId { get; set; }
+
+        [Required]
+        public int ConsultorioId { get; set; }
+    }
+}
