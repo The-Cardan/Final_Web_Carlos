73b894ee115b368757cf9ba46ff58526ce22018c
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:45:15 +0200

    Create DentistaUpdateDto.cs

---
+++ DTOs/DentistaUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class DentistaUpdateDto
+    {
+        [Required]
+        [StringLength(120)]
+        public string NombreCompleto { get; set; } = string.Empty;
+
+        [Required]
+        public string Telefono { get; set; } = string.Empty;
+
+        public string? Correo { get; set; }
+
+        [Required]
+        public int EspecialidadId { get; set; }
+    }
+}
