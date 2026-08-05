b088b04b42f246825f18b1d269821545262a5f7b
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:04:43 +0200

    Create IHorarioDentistaRepository.cs

---
+++ Interfaces/IHorarioDentistaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IHorarioDentistaRepository
+    {
+        Task<List<HorarioDentista>> ObtenerTodosAsync();
+
+        Task<HorarioDentista?> ObtenerPorIdAsync(int id);
+
+        Task<HorarioDentista> CrearAsync(HorarioDentista horario);
+
+        Task<HorarioDentista> ActualizarAsync(HorarioDentista horario);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
