23b541c673f2a6cf84f361e8c0fa63a084235500
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:53:58 +0200

    Create IMotivoRepository.cs

---
+++ Repositories/IMotivoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IMotivoRepository
+    {
+        Task<List<Motivo>> ObtenerTodosAsync();
+
+        Task<Motivo?> ObtenerPorIdAsync(int id);
+
+        Task<Motivo?> ObtenerPorDescripcionAsync(string descripcion);
+
+        Task<Motivo> CrearAsync(Motivo motivo);
+
+        Task<Motivo> ActualizarAsync(Motivo motivo);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
