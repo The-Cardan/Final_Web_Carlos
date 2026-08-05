251b388de1f4231c48d5aa95172542e455bfbbf1
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:23:32 +0200

    Create DiaSemana.cs

---
+++ Models/Enums/DiaSemana.cs
@@ -0,0 +1,13 @@
+﻿namespace Final_Web_Carlos.Models.Enums
+{
+    public enum DiaSemana
+    {
+        Domingo,
+        Lunes,
+        Martes,
+        Miercoles,
+        Jueves,
+        Viernes,
+        Sabado
+    }
+}
