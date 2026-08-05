44d277f6afdadaa8384e2cbb765bee1de17b185a
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:44:03 +0200

    Create ICitaService.cs

---
+++ Interfaces/ICitaService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface ICitaService
+    {
+        Task<List<CitaResponseDto>> ObtenerTodasAsync();
+
+        Task<CitaResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<CitaResponseDto> CrearAsync(CitaCreateDto dto);
+
+        Task<CitaResponseDto?> ActualizarAsync(int id, CitaUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
