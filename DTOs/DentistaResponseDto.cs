88c48a7fae983f220b3a03c7e9cbaf2349d67efd
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:46:02 +0200

    Create DentistaResponseDto.cs

---
+++ DTOs/DentistaResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class DentistaResponseDto
+    {
+        public int Id { get; set; }
+
+        public string NombreCompleto { get; set; } = string.Empty;
+
+        public string Telefono { get; set; } = string.Empty;
+
+        public string? Correo { get; set; }
+
+        public int EspecialidadId { get; set; }
+
+        public string Especialidad { get; set; } = string.Empty;
+    }
+}
