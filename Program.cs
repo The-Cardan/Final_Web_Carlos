7846b09d256cb9d816d491a8627c1237eb8e75a7
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:09:17 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -68,6 +68,9 @@ builder.Services.AddScoped<IEspecialidadService, EspecialidadService>();
 builder.Services.AddScoped<IDentistaRepository, DentistaRepository>();
 builder.Services.AddScoped<IDentistaService, DentistaService>();
 
+builder.Services.AddScoped<IHorarioDentistaRepository, HorarioDentistaRepository>();
+builder.Services.AddScoped<IHorarioDentistaService, HorarioDentistaService>();
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
