0ff1c8210cfc8523f8ba800715e0ffb2dc8c8cbd
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:57:03 +0200

    Create IMotivoService.cs

---
+++ Interfaces/IMotivoService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IMotivoService
+    {
+        Task<List<MotivoResponseDto>> ObtenerTodosAsync();
+
+        Task<MotivoResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<MotivoResponseDto> CrearAsync(MotivoCreateDto dto);
+
+        Task<MotivoResponseDto?> ActualizarAsync(int id, MotivoUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
