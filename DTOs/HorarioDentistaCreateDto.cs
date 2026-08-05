740e64d5109b557a5c99df2778029879027414ef
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:02:11 +0200

    Create HorarioDentistaCreateDto.cs

---
+++ DTOs/HorarioDentistaCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class HorarioDentistaCreateDto
+    {
+        [Required]
+        public int DentistaId { get; set; }
+
+        [Required]
+        public DayOfWeek Dia { get; set; }
+
+        [Required]
+        public TimeSpan HoraInicio { get; set; }
+
+        [Required]
+        public TimeSpan HoraFin { get; set; }
+    }
+}
