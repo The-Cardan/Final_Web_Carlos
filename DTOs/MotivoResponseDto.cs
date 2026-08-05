82d70488e491412ea5f35b77ce36d4a1ef5eada2
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:53:00 +0200

    Create MotivoResponseDto.cs

---
+++ DTOs/MotivoResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class MotivoResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
