8130bc43c30e0d91cd3db87778278aed0ec5e41c
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:25:22 +0200

    Create EspecialidadUpdateDto.cs

---
+++ DTOs/EspecialidadUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class EspecialidadUpdateDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
