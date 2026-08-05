608cb8eafc4be93cf326456697d2cf53e0f07546
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:28:44 +0200

    Create IEspecialidadRepository.cs

---
+++ Interfaces/IEspecialidadRepository.cs
@@ -0,0 +1,19 @@
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IEspecialidadRepository
+    {
+        Task<List<Especialidad>> ObtenerTodasAsync();
+
+        Task<Especialidad?> ObtenerPorIdAsync(int id);
+
+        Task<Especialidad?> ObtenerPorNombreAsync(string nombre);
+
+        Task<Especialidad> CrearAsync(Especialidad especialidad);
+
+        Task<Especialidad> ActualizarAsync(Especialidad especialidad);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
