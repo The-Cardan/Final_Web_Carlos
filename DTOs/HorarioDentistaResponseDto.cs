99783fd6dac126825d653a15e8d73ced96844d0a
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:03:41 +0200

    Create HorarioDentistaResponseDto.cs

---
+++ DTOs/HorarioDentistaResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class HorarioDentistaResponseDto
+    {
+        public int Id { get; set; }
+
+        public int DentistaId { get; set; }
+
+        public string Dentista { get; set; } = string.Empty;
+
+        public DayOfWeek Dia { get; set; }
+
+        public TimeSpan HoraInicio { get; set; }
+
+        public TimeSpan HoraFin { get; set; }
+    }
+}
