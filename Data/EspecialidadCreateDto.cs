802c608c30aa0a990c4ab9f47fccf1d5b6b17dc0
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:22:24 +0200

    Create EspecialidadCreateDto.cs

---
+++ Data/EspecialidadCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class EspecialidadCreateDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
