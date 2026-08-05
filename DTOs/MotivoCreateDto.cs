c5685e94fc1011d70cf14c6ca5650e4f43339570
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 23:51:35 +0200

    Create MotivoCreateDto.cs

---
+++ DTOs/MotivoCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class MotivoCreateDto
+    {
+        [Required]
+        [StringLength(150)]
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
