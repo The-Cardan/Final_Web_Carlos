9d6ece55d7d9fde393504f2d5a602f4f7f13451e
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-05 22:32:10 +0200

    Create IEspecialidadService.cs

---
+++ Interfaces/IEspecialidadService.cs
@@ -0,0 +1,17 @@
+﻿using Final_Web_Carlos.DTOs;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IEspecialidadService
+    {
+        Task<List<EspecialidadResponseDto>> ObtenerTodasAsync();
+
+        Task<EspecialidadResponseDto?> ObtenerPorIdAsync(int id);
+
+        Task<EspecialidadResponseDto> CrearAsync(EspecialidadCreateDto dto);
+
+        Task<EspecialidadResponseDto?> ActualizarAsync(int id, EspecialidadUpdateDto dto);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
