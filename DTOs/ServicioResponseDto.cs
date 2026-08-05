f1ce96224eef76e55aa043705584b32ba0015cb7
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:07:54 +0200

    Create ServicioResponseDto.cs

---
+++ DTOs/ServicioResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class ServicioResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public decimal Precio { get; set; }
+    }
+}
