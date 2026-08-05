951ca133feab414a65d4242a35a93eb1513b4e54
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:17:49 +0200

    Create ConsultorioUpdateDto.cs

---
+++ DTOs/ConsultorioUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class ConsultorioUpdateDto
+    {
+        [Required]
+        [StringLength(80)]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
