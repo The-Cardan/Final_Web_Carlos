8c2591b1afccbfe6ac37dca30700fc5e3e55e505
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:08:33 +0200

    Create IServicioRepository.cs

---
+++ Interfaces/IServicioRepository.cs
@@ -0,0 +1,19 @@
+﻿using Final_Web_Carlos.Models;
+
+namespace Final_Web_Carlos.Interfaces
+{
+    public interface IServicioRepository
+    {
+        Task<List<Servicio>> ObtenerTodosAsync();
+
+        Task<Servicio?> ObtenerPorIdAsync(int id);
+
+        Task<Servicio?> ObtenerPorNombreAsync(string nombre);
+
+        Task<Servicio> CrearAsync(Servicio servicio);
+
+        Task<Servicio> ActualizarAsync(Servicio servicio);
+
+        Task<bool> EliminarAsync(int id);
+    }
+}
