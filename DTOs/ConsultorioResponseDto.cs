8f3957a3a487b982510b942a261114d0d9e57980
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-06 00:18:38 +0200

    Create ConsultorioResponseDto.cs

---
+++ DTOs/ConsultorioResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Final_Web_Carlos.DTOs
+{
+    public class ConsultorioResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
