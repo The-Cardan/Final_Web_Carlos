555da8f79741caf1bab6b79fc485df1162fc82bc
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-08-07 00:06:18 +0200

    Create UsuarioUpdateDto.cs

---
+++ DTOs/UsuarioUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Web_Carlos.DTOs
+{
+    public class UsuarioUpdateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Correo { get; set; } = string.Empty;
+
+        [MinLength(6)]
+        public string? Password { get; set; }
+    }
+}
